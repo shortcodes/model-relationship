feb7772ade3d35cff9067bbab6c496a1f4bc2210
roman.szymanski <r.szymanski@mindz.it>
2019-05-23 12:10:03 +0200

    feat() - chenge relationship are saved

--- src/Traits/Relationship.php
+++ src/Traits/Relationship.php
@@ -1,54 +1,171 @@
 <?php
 
 namespace Shortcodes\ModelRelationship\Traits;
-use ErrorException;
-use Illuminate\Database\Eloquent\Relations\Relation;
+
+use Illuminate\Support\Arr;
 use ReflectionClass;
-use ReflectionMethod;
-use Shortcodes\ModelRelationship\Observers\RelationObserver;
+use ReflectionException;
 
 trait Relationship
 {
-    use WithoutEvents;
-
-    public static function bootRelationship()
+    public function relations()
     {
-        static::observe(RelationObserver::class);
-    }
-
-    public function getNotObservable()
-    {
-        return [];
-    }
-
-    public function relationships()
-    {
-
         $model = new static;
 
-        $relationships = [];
+        $relations = [];
 
-        foreach ((new ReflectionClass($model))->getMethods(ReflectionMethod::IS_PUBLIC) as $method) {
-            if ($method->class != get_class($model) ||
-                !empty($method->getParameters()) ||
-                $method->getName() == __FUNCTION__) {
+        try {
+            $reflectionClass = new ReflectionClass(get_called_class());
+        } catch (ReflectionException $e) {
+            return $relations;
+        }
+
+        foreach ($reflectionClass->getMethods() as $method) {
+            $doc = $method->getDocComment();
+
+            if (!$doc || strpos($doc, '@relation') === false) {
                 continue;
             }
 
             try {
                 $return = $method->invoke($model);
 
-                if ($return instanceof Relation) {
-                    $relationships[$method->getName()] = [
-                        'type' => (new ReflectionClass($return))->getShortName(),
-                        'model' => (new ReflectionClass($return->getRelated()))->getName()
-                    ];
-                }
-            } catch (ErrorException $e) {
-
+                $relations[$method->getName()] = [
+                    'type' => (new ReflectionClass($return))->getShortName(),
+                    'model' => (new ReflectionClass($return->getRelated()))->getName(),
+                ];
+            } catch (ReflectionException $e) {
+                continue;
             }
         }
 
-        return $relationships;
+        return $relations;
     }
+
+    public static function create(array $attributes = [], $useOriginal = false)
+    {
+        if ($useOriginal) {
+            return static::query()->create($attributes);
+        }
+
+        $model = new static ();
+        $model->fill($attributes);
+
+        $model->fireModelEvent('saving');
+        $model->fireModelEvent('creating');
+
+        $createdModel = static::withoutEvents(function () use ($model, $attributes) {
+
+            $model = tap($model)->save();
+            static::handleRelations($model, $attributes);
+
+            return $model->refresh();
+        });
+
+        $createdModel->fireModelEvent('created');
+        $createdModel->fireModelEvent('saved');
+
+        return $createdModel;
+    }
+
+    public function update(array $attributes = [], $useOriginal = false)
+    {
+        if (!$this->exists) {
+            return false;
+        }
+
+        if ($useOriginal) {
+            return $this->fill($attributes)->save([]);
+        }
+
+
+        $this->fireModelEvent('saving');
+        $this->fireModelEvent('updating');
+
+        $model = static::withoutEvents(function () use ($attributes) {
+
+            $this->fill($attributes)->save();
+
+            static::handleRelations($this, $attributes);
+
+            return $this->refresh();
+        });
+
+
+        $model->fireModelEvent('created');
+        $model->fireModelEvent('saved');
+
+        return $model;
+
+    }
+
+    private static function handleRelations($model, array $attributes = [])
+    {
+        $relationships = $model->relations();
+
+
+        foreach ($attributes as $k => $object) {
+
+            if (!isset($relationships[$k])) {
+                continue;
+            }
+
+            $method = "save" . $relationships[$k]['type'];
+            $relationships[$k]['name'] = $k;
+            static::$method($model, $relationships[$k], $object);
+        }
+
+    }
+
+    private static function saveBelongsTo($model, $relation, $object)
+    {
+        $relationObject = $relation['model']::find($object['id']);
+        $relationName = $relation['name'];
+        $model->$relationName()->associate($relationObject);
+        $model->save();
+
+    }
+
+    private static function saveHasOne($model, $relation, $object)
+    {
+        $relationObject = new $relation['model']($object);
+        $relationName = $relation['name'];
+
+        if ($model->$relationName) {
+            $model->$relationName->update($object);
+            return;
+        }
+
+        $model->$relationName()->save($relationObject);
+    }
+
+    private static function saveHasMany($model, $relation, $object)
+    {
+        $objectsCollection = collect($object);
+        $relationName = $relation['name'];
+        $idsNotToDelete = $objectsCollection->where('id', '!=', null)
+            ->pluck('id')->toArray();
+
+        $model->$relationName()->whereNotIn('id', $idsNotToDelete)->each(function ($item) use ($model, $relationName) {
+            $model->$relationName()->where('id', $item['id'])->first()->delete();
+        });
+
+        $objectsCollection->where('id', '!=', null)->each(function ($item) use ($model, $relationName) {
+            $model->$relationName()->where('id', $item['id'])->first()->update($item);
+        });
+
+        $model->$relationName()->createMany($objectsCollection->where('id', '=', null)->toArray());
+    }
+
+    private static function saveBelongsToMany($model, $relation, $object)
+    {
+        $objectsCollection = collect($object);
+        $relationName = $relation['name'];
+        $model->$relationName()->sync($objectsCollection->keyBy('id')->map(function ($item) {
+            return Arr::except($item, ['id']);
+        }));
+
+    }
+
+
 }
