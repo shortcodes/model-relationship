57656c5e7a44c8283d37b8666674de710e0128a2
Roman Szymański <r.szymanski@mindz.pl>
2019-07-11 21:33:47 +0200

    fix(traits): Fix created event execution

--- src/Traits/Relationship.php
+++ src/Traits/Relationship.php
@@ -93,7 +93,7 @@ trait Relationship
         });
 
 
-        $model->fireModelEvent('created');
+        $model->fireModelEvent('updated');
         $model->fireModelEvent('saved');
 
         return $model;
